a2f3811905ad5816d4f7ad56d515c92071893333
Mike Petruniak <mike.petruniak@gmail.com>
2018-01-13 23:24:10 -0500

    Mobile-friendly fix. Comments.

--- index.php
+++ index.php
@@ -6,6 +6,8 @@ require_once('src/actionController.php');
 <html>
 <head>
 <title>Blackjack-php</title>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
 <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.min.css" />
 <link rel="stylesheet" href="/public/css/style.css" />
 </head>
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,7 @@
 <?php
 require_once(dirname(__DIR__) . '/vendor/autoload.php');
+
+// Required for Travis
 require_once(dirname(__DIR__) . '/src/mykisscool/Blackjack/Card.php');
 require_once(dirname(__DIR__) . '/src/mykisscool/Blackjack/PlayerActions.php');
 require_once(dirname(__DIR__) . '/src/mykisscool/Blackjack/Player.php');
